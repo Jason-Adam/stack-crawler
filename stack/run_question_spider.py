7dd0cf7a9ef0eb22102e68b48b5a5a502b2bf53f
Jason Adam <22adamj22@gmail.com>
2019-10-12 23:10:19 -0700

    updated run_question_spider.py

--- stack/run_question_spider.py
+++ stack/run_question_spider.py
@@ -1,8 +1,10 @@
 from twisted.internet import reactor
 from scrapy.utils.project import get_project_settings
+from scrapy.utils.log import configure_logging
 from scrapy.crawler import CrawlerRunner
 from stack.spiders.question_spider import QuestionSpider
 
+configure_logging({"LOG_FORMAT": "%(levelname)s: %(message)s"})
 SETTINGS = get_project_settings()
 RUNNER = CrawlerRunner(SETTINGS)
 
